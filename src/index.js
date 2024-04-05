7d5abefa379de8e4a27d228c9698d139858b0996
allan santos <allan.santos21@outlook.com>
2024-04-05 11:36:25 -0400

    arquivos da pasta src removidos, e estrutura básica adicionada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Board() {
+      return (
+        <div>
+        <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'></button>
+        <button className='square'></button>
+        </div>  
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+        <div>
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+        </div>
+
+        </div>
+
+      )
+}
+
+
+function Game(){
+      return (
+        <div className='game'>
+          <div className='game-board'>
+          <Board/>
+          </div>
+  
+          <div className='game-info'>
+            Jogo da Velha
+          </div>
+        </div>
+
+  ) 
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <Game/>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
